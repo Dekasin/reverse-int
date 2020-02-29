3283f99e1a1fc4919708fa6a00ac2d7a3f7f5027
dekasins <dekasins@gmail.com>
2020-02-29 13:21:08 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+  //  console.log(arr); //check arr value
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+      newArr[i] = arr[arr.length-i-1];
+  }
+  //  console.log(newArr); // check nawArr value
+  return parseInt(newArr.join(""));
 }
